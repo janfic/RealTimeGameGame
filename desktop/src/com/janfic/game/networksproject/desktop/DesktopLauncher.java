2941857ba2d1fcd052ff1bea82ed45d522be6c4d
Jan Fic <greyjan.fic@gmail.com>
2019-05-13 16:57:05 -0400

    added icon

--- desktop/src/com/janfic/game/networksproject/desktop/DesktopLauncher.java
+++ desktop/src/com/janfic/game/networksproject/desktop/DesktopLauncher.java
@@ -1,17 +1,24 @@
 package com.janfic.game.networksproject.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.janfic.game.networksproject.RealTimeGameGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-                config.width = 1400;
-                config.height = 800;
-                config.foregroundFPS = 60;
-                config.resizable = false;
-                //config.fullscreen = true;
-		new LwjglApplication(new RealTimeGameGame(), config);
-	}
+
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 1400;
+        config.height = 800;
+        config.foregroundFPS = 60;
+        config.resizable = false;
+        config.title = "Real-Time Game Game";
+        if (System.getProperty("os.name").contains("mac")) {
+            config.addIcon("icon-mac.png", Files.FileType.Internal);
+        } else {
+            config.addIcon("icon.png", Files.FileType.Internal);
+        }
+        new LwjglApplication(new RealTimeGameGame(), config);
+    }
 }
